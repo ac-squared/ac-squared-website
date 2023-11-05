e8d16b24cee6aa8710c269ae86867f8ce3bce695
Tait Berlette <54515877+taitberlette@users.noreply.github.com>
2023-11-05 12:22:18 -0500

    Update homepage with new!
    Added links, about the club, about the execs, featured projects, and the calendar! Featured projects & calendar are updated from the server on page load!

--- public/source/index.js
+++ public/source/index.js
@@ -1,3 +1,146 @@
 // index.js
 
-console.log('hello world')
+// get the project elements
+const projectLoading = document.getElementById("projects-loading")
+const projectGrid = document.getElementById("projects-grid")
+
+// get the calendar elements
+const calendarLoading = document.getElementById("calendar-loading")
+const calendarGrid = document.getElementById("calendar-grid")
+
+// load the featured projects from the server
+const loadProjects = () => {
+  return new Promise(async (res, rej) => {
+    // request the api endpoint
+    const projectRequest = await fetch('./api/projects/featured')
+    const projectResponse = await projectRequest.json()
+
+    // if the request is not ok show an error message
+    if(projectRequest.status != 200) {
+      projectLoading.innerText = projectResponse.error ?? `Failed to load! 😭`
+
+      rej(new Error('Failed to load projects!'))
+      return;
+    }
+
+    // get the array of featured projects
+    const featured = projectResponse.featured
+
+    // if there's none show the error message
+    if(featured.length == 0) {
+      projectLoading.innerText = `No projects are featured right now! 🧑‍💻`
+      res()
+      return;
+    }
+    
+    const elements = []
+    
+    // create the elements for all projects
+    for(const feature of featured) {
+      const element = document.createElement("div")
+      element.classList.add("project")
+
+      const name = document.createElement("h2")
+      name.innerText = feature.name
+      element.appendChild(name)
+
+      const info = document.createElement("p")
+      info.innerText = feature.description
+      element.appendChild(info)
+
+      const link = document.createElement("a")
+      link.classList.add("button")
+      link.target = "_blank"
+      link.href = feature.url
+      link.innerText = "Repository"
+      element.appendChild(link)
+      
+      elements.push(element)
+    }
+    
+    // add them to the grid
+    projectGrid.replaceChildren(...elements)
+
+    // switch from loading to the grid of projects
+    projectLoading.style.display = "none"
+    projectGrid.style.display = "grid"
+
+    res()
+  })
+}
+
+// load the upcoming calendar events from the server
+const loadCalendar = () => {
+  return new Promise(async (res, rej) => {
+    // request the api endpoint
+    const calendarRequest = await fetch('./api/calendar/events')
+    const calendarResponse = await calendarRequest.json()
+
+    // if the request is not ok show an error message
+    if(calendarRequest.status != 200) {
+      calendarLoading.innerText = calendarResponse.error ?? `Failed to load! 😭`
+
+      rej(new Error('Failed to load calendar!'))
+      return;
+    }
+
+    // get the array of calendar events
+    const events = calendarResponse.events
+
+    // if there's none show the error message
+    if(events.length == 0) {
+      calendarLoading.innerText = `No events upcoming! 😎`
+      res()
+      return;
+    }
+
+    const elements = []
+
+    // create the elements for all events
+    for(const event of events) {
+      const element = document.createElement("div")
+      element.classList.add("calendar")
+
+      const info = document.createElement("p")
+      info.classList.add("calendar-info")
+      element.appendChild(info)
+  
+      const title = document.createElement("span")
+      title.classList.add("calendar-title")
+      title.innerText = event.name
+      info.appendChild(title)
+
+      const place = document.createElement("span")
+      place.innerText = ` at ${event.location}`
+      info.appendChild(place)
+
+      const date = document.createElement("p")
+      date.classList.add("calendar-date")
+      element.appendChild(date)
+
+      const time = document.createElement("span")
+      time.innerText = `${event.date} at ${event.time}`
+      date.appendChild(time)
+
+      elements.push(element)
+    }
+    
+    // add them to the grid
+    calendarGrid.replaceChildren(...elements)
+
+    // switch from loading to the grid of events
+    calendarLoading.style.display = "none"
+    calendarGrid.style.display = "grid"
+
+    res()
+  })
+}
+
+// load all data from the server
+const start = async () => {
+  await loadProjects()
+  await loadCalendar()
+}
+
+// await the document being ready before starting!
+document.addEventListener('DOMContentLoaded', start)
